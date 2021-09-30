72cbd1e330436922aa48d54c4781c2fb84efb8b1
Skunkspray <91703828+Skunkspray@users.noreply.github.com>
2021-09-30 18:49:26 -0400

    Add files via upload

---
+++ ForLoopExample.java
@@ -0,0 +1,16 @@
+// Jeff Blankenship
+// CS-151
+// for loop example
+
+public class ForLoopExample {
+
+  public static void main(String[] args){
+  
+  //suppose you want to print something 5 times
+  for (int count = 0; count<5; count++){
+    System.out.println("something " + count);
+  }
+  
+  
+  }
+}
